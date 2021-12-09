8aad051a925ac9886f9ad2a5b559add1d68bcb62
Meng Luan <edmonluan@gmail.com>
2021-12-09 14:20:32 -0500

    add dummy implementation

--- index.ts
+++ index.ts
@@ -6,5 +6,8 @@ export default function <T extends (...args: any[]) => any> (
     job: T,
     config: Config = {},
 ): (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> {
-  // TODO: not implemented yet
+  // TODO: this is not correctly implemented.
+
+  config.limit;
+  return async (...args) => job(...args);
 }
